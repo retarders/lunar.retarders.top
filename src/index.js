a0dd6000cc4b7480f128c5aca7a08ab450681aee
w <pcranaway@tuta.io>
2022-05-03 12:45:38 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ function createURL(data) {
     return `${endpoint}${data.path}?v=${data.videoId}`;
 }
 
-setTimeout(() => document.location.href = createURL({ path: 'watch', videoId: 'dQw4w9WgXcQ' }), 1000);
+setTimeout(() => document.location.href = createURL({ path: 'watch', videoId: 'dQw4w9WgXcQ' }), 100);
